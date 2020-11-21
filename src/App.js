528face265a3e5fa2b93659f7b75a0026c96a368
sid2015980 <sidneybroadnax@gmail.com>
2020-11-20 22:57:41 -0700

    Finished... had to deal with errors

--- src/App.js
+++ src/App.js
@@ -1,34 +1,58 @@
-import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
-import data from './data';
+import React, { useState, useEffect } from "react";
+import { Route } from "react-router-dom";
+import data from "./data";
 
 // Components
-import Navigation from './components/Navigation';
-import Products from './components/Products';
-import ShoppingCart from './components/ShoppingCart';
+import Navigation from "./components/Navigation";
+import Products from "./components/Products";
+import ShoppingCart from "./components/ShoppingCart";
+
+//context
+import { CartContext } from "../src/context/CartContext";
+import { ProductContext } from "../src/context/ProductContext";
 
 function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+  const [products] = useState(data);
+  const [cart, setCart] = useState([]);
 
-	const addItem = item => {
-		// add the given item to the cart
-	};
+  const addItem = (item) => {
+    setCart([...cart, item]);
+  };
 
-	return (
-		<div className="App">
-			<Navigation cart={cart} />
+  const removeItem = (item) => {
+    const filtered = cart.filter((selection) => selection.id !== item.id);
+    setCart(filtered);
+  };
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+  useEffect(() => {
+    const checkCart = JSON.parse(localStorage.getItem("cart"));
+    if (checkCart) {
+      setCart(checkCart);
+    }
+  }, []);
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
-		</div>
-	);
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
+  return (
+    <div className="App">
+      <CartContext.Provider value={{ cart, removeItem }}>
+        <ProductContext.Provider value={{ products, addItem }}>
+          <Navigation cart={cart} />
+
+          {/* Routes */}
+          <Route exact path="/">
+            <Products />
+          </Route>
+
+          <Route path="/cart">
+            <ShoppingCart cart={cart} />
+          </Route>
+        </ProductContext.Provider>
+      </CartContext.Provider>
+    </div>
+  );
 }
 
 export default App;
--- src/data.js
+++ src/data.js
@@ -1,27 +1,29 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export default [
 	{
-		id: 1,
+		id: uuidv4(),
 		title: 'The Art Of War',
 		price: 10.11,
 		image:
 			'https://images-na.ssl-images-amazon.com/images/I/41FBMkY3cgL._SX331_BO1,204,203,200_.jpg'
 	},
 	{
-		id: 2,
+		id: uuidv4(),
 		title: 'Civilization and Its Discontents',
 		price: 14.29,
 		image:
 			'https://images-na.ssl-images-amazon.com/images/I/51Jj12iMZnL._SX331_BO1,204,203,200_.jpg'
 	},
 	{
-		id: 3,
+		id: uuidv4(),
 		title: 'Influence: The Psychology of Persuasion',
 		price: 10.98,
 		image:
 			'https://images-na.ssl-images-amazon.com/images/I/512-B-1yXuL._SX331_BO1,204,203,200_.jpg'
 	},
 	{
-		id: 4,
+		id: uuidv4(),
 		title: 'Grokking Algorithms',
 		price: 40.00,
 		image:
